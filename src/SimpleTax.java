17be32c887510d819516eef098f391c339bc0876
ewbrowntech <ewbrowntech@gmail.com>
2021-11-21 07:47:54 -0600

    Added program driver file

---
+++ src/SimpleTax.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+/**
+ * SimpleTax.java 
+ * 
+ * SimpleTax program. Simple application for filing of taxes.
+ *
+ * @author   Ethan Brown (ewb0020@auburn.edu)
+ * @version  21 NOV 21
+ */
+
+public class SimpleTax {
+	
+	public static void main(String[] args) 
+			throws java.io.IOException {
+		
+		    HomeScreen h = new HomeScreen();
+		    h.welcomePrompt();		// Calls up the homescreen, with its options
+	}
+}
