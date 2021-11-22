34912019549fc248621bb7812cb918d834fc3f84
ewbrowntech <ewbrowntech@gmail.com>
2021-11-21 18:08:20 -0600

    Added User class in User.java

---
+++ src/user.java
@@ -0,0 +1,44 @@
+/**
+ * User.java
+ *
+ * Simple user class
+ *
+ * @author   Ethan Brown (ewb0020@auburn.edu)
+ * @version  21 NOV 21
+ */
+public class user {
+	String username;
+	String password;
+	Boolean isAdmin;
+	Double income;
+	Double deductions;
+	Double balance;
+
+	public void setUsername(String usernameIn) {
+		this.username = usernameIn;
+	}
+
+	public void setPassword(String passwordIn) {
+		this.password = passwordIn;
+	}
+
+	public void makeAdmin() {
+		this.isAdmin = true;
+	}
+
+	public void makeNotAdmin() {
+		this.isAdmin = false;
+	}
+
+	public void setIncome(Double incomeIn) {
+		this.income = incomeIn;
+	}
+
+	public void setDeductions(Double deductionsIn) {
+		this.deductions = deductionsIn;
+	}
+
+	public void setBalance(Double balanceIn) {
+		this.balance = balanceIn;
+	}
+}
